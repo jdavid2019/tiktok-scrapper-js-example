9901105f598a59ac8c73a076ad7899e6be265204
Joseph David Ninaquispe Florez <54923488+jdavid2019@users.noreply.github.com>
2021-10-27 17:23:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
 const TikTokScraper = require('tiktok-scraper');
 
-
-//https://github.com/drawrowfly/tiktok-scraper
-
-// Search hastags
-/*(async () => {
+// Get posts related with a specific hashtag. You need to pass hashtag and the number on posts that you want to see.
+(async () => {
     try {
         const posts = await TikTokScraper.hashtag('#milydance', { number: 100 });
         console.log(posts);
     } catch (error) {
         console.log(error);
     }
-})();*/
+})();
 
 
-// User information
-/*(async () => {
+// Get user profile information. You need to pass tiktok username. e.g: milydance.oficial
+(async () => {
     try {
         const user = await TikTokScraper.getUserProfileInfo('milydance.oficial');
         console.log(user);
     } catch (error) {
         console.log(error);
     }
-})();*/
+})();
 
-// Get single video metadata
-// input - WEB_VIDEO_URL
-// For example: https://www.tiktok.com/@milydance.oficial/video/6992614939575799045
-// options - not required
+// Get single video metadata. You need to pass web video tiktok url. e.g: https://www.tiktok.com/@milydance.oficial/video/6992614939575799045
+
 (async () => {
     try {
         const videoMeta = await TikTokScraper.getVideoMeta('https://www.tiktok.com/@milydance.oficial/video/6992614939575799045');
